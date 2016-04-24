93399371f4a28387b6f2860bb7dae99b7f83bd4d
GrayChen <455803034@qq.com>
2016-04-24 23:10:42 +0800

    bug:修正登录的bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 /*Route::group(['namespace' => 'Home'], function(){
    Route::get('home/test','TestController@index');
    Route::get('home/service','ServiceController@index');
@@ -50,10 +50,10 @@ Route::group(['prefix'=>'Admin','namespace' => 'Admin'],function(){
 	Route::get('test','TestController@index');
 });
 // 认证路由...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@getLogout');
 // 注册路由...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
-Route::get('/home', 'HomeController@index');
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+Route::get('home', 'HomeController@index');
