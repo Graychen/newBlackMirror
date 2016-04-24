2b2d33bdc89abb7817bbfa4938ad61fe53da9bf3
GrayChen <455803034@qq.com>
2016-04-24 23:26:14 +0800

    test:远程仓库修改测试

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,7 @@
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
+
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                             </ul>
